a31c9664eab245148dfc42fc5e05a15239712c54
lemmy <lemmy@yandex-team.ru>
2015-09-01 13:35:05 +0300

    Fix install link

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -s https://raw.githubusercontent.com/baby-loris/baby-bem/master/get-a-pet.sh > /tmp/baby-bem-install-$$.sh
-sh /tmp/baby-bem-install-$$.sh < /dev/tty
-rm /tmp/baby-bem-install-$$.sh
